75ce646ff932bf5f581331208f2e5f19a1a3e2e9
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-04-14 20:22:11 -0700

    [update]

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,13 +2,35 @@ const Post = require('../models/post');
 
 module.exports = (app) => {
 
-  // CREATE
-  app.post('/posts/new', (req, res) => {
-    // INSTANTIATE INSTANCE OF POST MODEL
-    const post = new Post(req.body);
+app.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find({}).lean();
+    return res.render('posts-index', { posts });
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
-    // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
-    post.save(() => res.redirect('/'));
-  });
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new');
+})
+
+// CREATE
+app.post('/posts/new', (req, res) => {
+// INSTANTIATE INSTANCE OF POST MODEL
+const post = new Post(req.body);
+
+// SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
+post.save(() => res.redirect('/'));
+});
+
+// LOOK UP THE POST
+app.get('/posts/:id', async (req, res) => {
+    Post.findById(req.params.id).lean()
+      .then((post) => res.render('posts-show', { post }))
+      .catch((err) => {
+        console.log(err.message);
+      });
+});
 
 };
