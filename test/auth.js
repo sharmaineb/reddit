d3cd82bf354216636cb7cfe9a3dda71511c008fe
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-04-27 13:35:02 -0700

    fixed posts test

--- test/auth.js
+++ test/auth.js
@@ -4,6 +4,7 @@ const chaiHttp = require('chai-http');
 const { describe, it } = require('mocha');
 const app = require('../server');
 
+
 const should = chai.should();
 
 chai.use(chaiHttp);
--- test/posts.js
+++ test/posts.js
@@ -1,24 +1,44 @@
 // test/posts.js
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { describe, it } = require('mocha');
-chai.use(chaiHttp);
+const { describe, it, before } = require('mocha');
 const app = require('../server');
 const agent = chai.request.agent(app);
 
 // Import the Post model from our models folder so we
 // we can use it in our tests.
 const Post = require('../models/post');
+const User = require('../models/user');
 
 const should = chai.should();
 
-describe('Posts', () => {
+chai.use(chaiHttp);
+
+describe('Posts', function () {
   // Post that we'll use for testing purposes
   const newPost = {
     title: 'post title',
     url: 'https://www.google.com',
-    summary: 'post summary'
+    summary: 'post summary',
+    subredditt: 'test'
   };
+  // User that we'll use for testing purposes
+  const user = {
+    username: 'poststest',
+    password: 'testposts',
+};
+before(function (done) {
+  agent
+    .post('/sign-up')
+    .set('content-type', 'application/x-www-form-urlencoded')
+    .send(user)
+    .then(function (res) {
+      done();
+    })
+    .catch(function (err) {
+      done(err);
+    });
+});
   it('Should create with valid attributes at POST /posts/new', (done) => {
     // Checks how many posts there are now
     Post.estimatedDocumentCount()
@@ -50,8 +70,25 @@ describe('Posts', () => {
       .catch((err) => {
           done(err);
       });
-      after(() => {
-        Post.findOneAndDelete(newPost);
+      after(function (done) {
+        Post.findOneAndDelete(newPost)
+        .then(function () {
+          agent.close();
+      
+          User
+            .findOneAndDelete({
+              username: user.username,
+            })
+            .then(function () {
+              done();
+            })
+            .catch(function (err) {
+              done(err);
+            });
+        })
+        .catch(function (err) {
+          done(err);
+        });
       });
       
 });
