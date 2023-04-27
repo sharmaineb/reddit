059657950ddb98abe4072c1ea2235d744b0762dd
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-04-27 13:50:40 -0700

    implemented nested comments

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(checkAuth);
 require('./controllers/posts')(app);
 require('./controllers/comments.js')(app);
 require('./controllers/auth.js')(app);
+require('./controllers/replies.js')(app);
 
 
 // Set db
